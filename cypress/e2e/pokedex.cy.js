21ee040995b81aa614b48d051600c784e11e1941
Haruta Tasaki <hrtune@gmail.com>
2023-03-15 21:41:21 +0900

    Add test for one pokemon page (11.9)

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,15 @@
+const homeUrl = 'http://localhost:5001' // port number of 5000 is already used by my mac os
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5001') // port number of 5000 is already used by my mac os
+    cy.visit(homeUrl)
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('ivysaur page can be opened', () => {
+    cy.visit(homeUrl)
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
   })
 })
