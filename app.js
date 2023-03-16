105417eee778dd9c9a5204e1a6ff2d53db92bd69
Haruta Tasaki <hrtune@gmail.com>
2023-03-16 22:23:14 +0900

    Implement simple health check by fly.io (11.11)

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+// for health check by fly.io
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+// notify the version of this app
+app.get('/version', (req, res) => {
+  res.send('5')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
